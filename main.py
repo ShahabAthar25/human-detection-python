2b9c7a0746799fbe580499748e12d70810fa23a6
ShahabAthar25 <shahabathar25@gmail.com>
2021-10-01 23:36:42 +0500

    Add Pose Detection

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import cv2
-import modules.pose_tracker
+from modules.pose_tracker import pose_detector
 
 cap = cv2.VideoCapture(0)
+detector_pose = pose_detector()
 
 while True:
     success, img = cap.read()
 
-
+    detector_pose.find_pose(img)
 
     cv2.imshow("Human Detector", img)
 
