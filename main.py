857ae574991756bd8c7ffaa4a372f9d5e1d79073
ShahabAthar25 <shahabathar25@gmail.com>
2021-10-01 23:52:10 +0500

    Add specific pose point detection

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ while True:
     success, img = cap.read()
 
     detector_pose.find_pose(img)
+    lm_list = detector_pose.get_specific_pose(img)
+    cv2.circle(img, (lm_list[14][1], lm_list[14][2]), 15, (255, 0, 0), cv2.FILLED)
 
     cv2.imshow("Human Detector", img)
 
