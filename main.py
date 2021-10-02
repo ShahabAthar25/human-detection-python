f71e1679e0269c6da81790bd31969a6e6beaa373
ShahabAthar25 <shahabathar25@gmail.com>
2021-10-02 14:24:08 +0500

    Add Hand Detector

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from modules.pose_tracker import pose_detector
 
 cap = cv2.VideoCapture(0)
 detector_pose = pose_detector()
+detector_hand = pose_detector()
 
 while True:
     success, img = cap.read()
 
     detector_pose.find_pose(img)
-    lm_list = detector_pose.get_specific_pose(img)
-    cv2.circle(img, (lm_list[14][1], lm_list[14][2]), 15, (255, 0, 0), cv2.FILLED)
+    detector_hand.find_pose(img)
 
     cv2.imshow("Human Detector", img)
 
